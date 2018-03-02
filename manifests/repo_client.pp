06545bc57f8abec2badb589e79d5e20e61b7a3f4
Dan Deremiah <daniel.deremiah@windriver.com>
2018-03-02 13:42:22 -0800

    updated the repo

--- manifests/repo_client.pp
+++ manifests/repo_client.pp
@@ -1,3 +1,4 @@
+
 class spacewalk::repo_client
 {
   case $::osfamily {
@@ -5,20 +6,22 @@ class spacewalk::repo_client
           case $::operatingsystemrelease {
             /^5/: {
               exec {'setupSpacewalkClientRepo':
-                cwd => '/etc/yum.repos.d', 
+                cwd => '/etc/yum.repos.d',
                 path   => "/usr/bin:/usr/sbin:/bin",
                 creates => '/etc/yum.repos.d/spacewalk-client.repo',
-                command => 'rpm -Uvh http://yum.spacewalkproject.org/2.0-client/RHEL/5/x86_64/spacewalk-client-repo-2.0-3.el5.noarch.rpm'
+                command => 'rpm -Uvh http://yum.spacewalkproject.org/2.7-client/RHEL/5/x86_64/spacewalk-client-repo-2.7-2.el5.noarch.rpm'
               }
             }
             /^6/: {
               exec {'setupSpacewalkClientRepo':
-                cwd     => '/etc/yum.repos.d', 
-                path    => "/usr/bin:/usr/sbin:/bin",
+                cwd     => '/etc/yum.repos.d',
+                path    => '/usr/bin:/usr/sbin:/bin',
                 creates => '/etc/yum.repos.d/spacewalk-client.repo',
-                command => 'rpm -Uvh http://yum.spacewalkproject.org/2.0-client/RHEL/6/x86_64/spacewalk-client-repo-2.0-3.el6.noarch.rpm'
+                command => 'rpm -Uvh http://yum.spacewalkproject.org/2.7-client/RHEL/6/x86_64/spacewalk-client-repo-2.7-2.el6.noarch.rpm'
               }
-              
+            }
+            default: {
+              fail("OS family ${::osfamily} not supported")
             }
           } # End OS Release Case
         }
@@ -26,5 +29,5 @@ class spacewalk::repo_client
             fail("OS family ${::osfamily} not supported")
         } # Since spacewalk is mostly good for just RH type systems, we restrict accordinly.
     } # End case on OS Family
-  
+
 }
