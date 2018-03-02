f34c62a3e55740ff92ba1fc8ddc0476267d4904c
Dan Deremiah <daniel.deremiah@windriver.com>
2018-03-02 15:25:00 -0800

    added cert download

--- manifests/client.pp
+++ manifests/client.pp
@@ -9,25 +9,33 @@ class spacewalk::client (
   #   spacewalk_fqdn => 'spacewalk.local.net',
   #   activation_key => '1-d9e796114e1e8ef073b605341ee6580d',
   # }
-    
+
   include spacewalk::repo_client
-  
+
   # spacewalk client packages needed
-  $packageList = ['rhn-client-tools','rhn-check', 'rhn-setup', 'm2crypto', 'yum-rhn-plugin']
-  
+  $packageList = ['rhn-client-tools','rhn-check', 'rhn-setup', 'm2crypto', 'yum-rhn-plugin', 'wget' ]
+
   package {$packageList:
     ensure => installed,
     require => Exec['setupSpacewalkClientRepo'],
   }
-  
+
+  exec {'downloadCert':
+    cwd => '/root',
+    path => '/usr/bin:/usr/sbin:/bin',
+    creates => '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT',
+    command => "wget -O /usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT http://$spacewalk_fqdn/pub/RHN-ORG-TRUSTED-SSL-CERT",
+    require => Package[$packageList],
+  }
+
   # Exec to register with the spacewalk server
   exec {'registerSpacewalk':
     cwd => '/root',
     path => '/usr/bin:/usr/sbin:/bin',
     creates => '/etc/sysconfig/rhn/systemid',
     command => "rhnreg_ks --serverUrl=http://$spacewalk_fqdn/XMLRPC --activationkey=$activation_key",
-    require => Package[$packageList],
+    require => Exec['downloadCert'],
   }
-  
-  
+
+
 }
